941cb29d20c7ea76c1fbd8147439b1a3f0694d3f
kashiwabara <moritamoriko@gmail.com>
2015-02-09 22:31:09 +0900

    自分のフォローの最新ツイートを取得

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,7 +1,18 @@
 class TimelinesController < ApplicationController
 
   def show
-
+    friends = twitter_client.friends("moriko_o", count: 5000)
+    @timeline = friends.find_all{|user| user.status.created_at.present? }.map(&:status).sort_by(&:created_at).reverse
   end
+
+  def twitter_client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        = ''
+      config.consumer_secret     = ''
+      config.access_token        = ''
+      config.access_token_secret = ''
+    end
+  end
+
 
 end

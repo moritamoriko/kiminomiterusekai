4254cf8d8a653fef8f753c208547cd67e19aaa03
Shoko Terajima <shokola.te.mocha@gmail.com>
2016-02-13 17:47:10 +0900

    TwitterのAPI制限を回避

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,7 +1,20 @@
 class TimelinesController < ApplicationController
 
+  SLICE_SIZE = 100
+
   def show
-    friends = twitter_client.friends("moriko_o", count: 5000)
+    def get_all_friends(screen_name)
+      client = twitter_client
+      all_friends = []
+      client.friend_ids(screen_name).each_slice(SLICE_SIZE).each do |slice|
+        client.users(slice).each do |friend|
+          all_friends << friend
+        end
+      end
+      all_friends
+    end
+
+    friends = get_all_friends("moriko_o")
     @timeline = friends.find_all{|user| user.status.created_at.present? }.map(&:status).sort_by(&:created_at).reverse
   end
 
@@ -13,6 +26,4 @@ class TimelinesController < ApplicationController
       config.access_token_secret = ''
     end
   end
-
-
 end

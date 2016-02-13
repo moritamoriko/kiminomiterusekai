47465afd1782f86cc4216cdffda1819e5b5ec9d7
moriko <hato3000wa@gmail.com>
2016-02-14 01:49:22 +0900

    accessTokenをsecret.ymlに移動

--- Gemfile
+++ Gemfile
@@ -25,7 +25,6 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'haml-rails'
 gem 'twitter'
-
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -20,10 +20,11 @@ class TimelinesController < ApplicationController
 
   def twitter_client
     Twitter::REST::Client.new do |config|
-      config.consumer_key        = ''
-      config.consumer_secret     = ''
-      config.access_token        = ''
-      config.access_token_secret = ''
+      config.consumer_key = Rails.application.secrets.consumer_key
+      config.consumer_secret = Rails.application.secrets.consumer_secret
+      config.access_token = Rails.application.secrets.access_token
+      config.access_token_secret = Rails.application.secrets.access_token_secret
+
     end
   end
 end

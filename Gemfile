66ac1ec9997e8b8471cd465b9b3ab9b74f9bc21c
moriko <hato3000wa@gmail.com>
2016-08-21 15:23:08 +0900

    add gem bootstrap-sass

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'sassc-rails'
 gem 'high_voltage'
 gem 'flutie'
 
+gem 'bootstrap-sass'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resource :timeline
+  root to: 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
